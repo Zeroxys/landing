cfe0d5f9c6170d92165966448010057c51b3740b
Miguel Zavala <miguelzavalac@gmail.com>
2017-05-20 14:14:33 -0500

    Automatic refresh on the build

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+/*---------> Postcss Plugins <------------*/
 const postcss = require('gulp-postcss')
 const cssnext = require('postcss-cssnext')
 const responsive = require('postcss-responsive-type')
@@ -8,8 +10,14 @@ const mixins = require('postcss-mixins')
 const mqpacker = require('css-mqpacker')
 const rename = require('gulp-rename')
 const sourcemaps = require('gulp-sourcemaps')
-const browserSync = require('browser-sync').create()
+/*----------> Javascript Build <------------*/
+const browserify = require('browserify')
+const source = require('vinyl-source-stream')
+const watchify = require('watchify')
+const babelify = require('babelify')
+const presets2015 = require('babel-preset-es2015')
 
+/*Browser Sync task server*/
 gulp.task('server', () => {
     browserSync.init({
         server: {
@@ -19,6 +27,8 @@ gulp.task('server', () => {
     })
 })
 
+
+/*Postcss task build*/
 gulp.task('css', () => {
   let processors = [atImport(),mixins,responsive,cssnext,mqpacker,csswring({removeAllComments:true})]
 
@@ -31,10 +41,67 @@ gulp.task('css', () => {
 
 })
 
+
+/*-----------------------------Build Function and task----------------------------------------------------*/
+
+function build(watch) {
+  let opts = {
+    entries: ['./src/js/index.js']
+  }
+
+    let build = watchify(browserify(opts))
+  
+  return new Promise( (resolve, reject) => {
+
+    function compile () {
+      return new Promise((resolve,reject) => {
+        build
+          .transform('babelify', {presets : ['es2015']})
+          .bundle()
+          .on('error', (err) => {
+              if(err){
+                return reject(err)
+              }
+            })
+          .pipe(source('index.js'))
+          .pipe(rename('build.js'))
+          .pipe(gulp.dest('./dist'))
+
+        resolve('Success...!')
+      }) 
+    }
+
+  if(watch){
+      return compile()
+              .then( result => {
+                console.log(`---------> Compile Status : ${result} <--------------`)
+              })
+              .catch( err => console.log(`---------> Compile Status : ${err} <--------------`))
+    }
+
+  })
+}
+
+/*Javascrip build task*/
+gulp.task('build', () => {
+  gulp.watch('./src/js/index.js').on('change',() => {
+    build(true).then(result => console.log("working")).catch( err => console.log("epic error.... lol !!!"))
+  })
+})
+
+/*-------------------------------------------------------------------------------------------------------*/
+
+/*Watch changes on html and css*/
 gulp.task('watcher', () => {
   gulp.watch('./dist/index.html').on('change', browserSync.reload)
   gulp.watch('./src/css/index.css', ['css']).on('change', browserSync.reload)
+  gulp.watch('./src/js/index.js').on('change',() => {
+    build(true)
+    browserSync.reload()
+  })
 })
 
+
+/*Default task*/
 gulp.task('default', ['server', 'watcher'])
 
